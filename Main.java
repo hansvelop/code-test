e4ad62708bd45816eed9c12de98cffb6ecce1edc
sintoro.han <sintoro.han@miracom-inc.com>
2024-01-09 13:21:41 +0900

    수정

--- Main.java
+++ Main.java
@@ -1,14 +1,13 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 class Main {
 
-    private static StringTokenizer st;
 
 
-    static class RockState{
+    static class RockState {
         int y;
         int x;
         int dir;
@@ -22,7 +21,7 @@ class Main {
         }
     }
 
-    static class Pos{
+    static class Pos {
         int y;
         int x;
 
@@ -32,29 +31,20 @@ class Main {
         }
     }
 
-    static int size;
-    static char[][] map;
-    static int[] dy = new int[]{-1,0,1,0};
-    static int[] dx = new int[]{0,1,0,-1};
-    public static void init(int N, char[][] mMap) {
-        size = N;
-        map = new char[size][size];
-        for(int y=0; y<size; y++){
-            for(int x=0; x<size; x++) {
-                map[y][x] = mMap[y][x];
-            }
-        }
-    }
+    static int _SIZE;
+    static char[][] _MAP;
+    static int[] _DY = new int[]{-1, 0, 1, 0};
+    static int[] _DX = new int[]{0, 1, 0, -1};
 
 
     public static int push(int mRockR, int mRockC, int mDir, int mGoalR, int mGoalC) {
         ArrayDeque<RockState> stoneQ = new ArrayDeque<>();
-        boolean[][][] stoneV = new boolean[size][size][4];
-        stoneV[mRockR][mRockC][mDir]=true;
+        boolean[][][] stoneV = new boolean[_SIZE][_SIZE][4];
         stoneQ.add(new RockState(mRockR, mRockC, mDir, 0));
-        while (!stoneQ.isEmpty()){
+        stoneV[mRockR][mRockC][mDir] = true;
+        while (!stoneQ.isEmpty()) {
             RockState stoneC = stoneQ.poll();
-            if(stoneC.y==mGoalR && stoneC.x==mGoalC){
+            if (stoneC.y == mGoalR && stoneC.x == mGoalC) {
                 return stoneC.cnt;
             }
             movePush(stoneQ, stoneV, stoneC);
@@ -62,123 +52,99 @@ class Main {
         return -1;
     }
 
-    private static void movePush(ArrayDeque<RockState> stoneQ, boolean[][][] stoneV, RockState stoneC){
-        int findCnt=0;
-        if(map[stoneC.y][stoneC.x+1]=='#' || map[stoneC.y][stoneC.x-1]=='#') findCnt+=2;
-        if(map[stoneC.y+1][stoneC.x]=='#' || map[stoneC.y-1][stoneC.x]=='#') findCnt+=2;
-        if(findCnt==4) return;
+    private static void movePush(ArrayDeque<RockState> stoneQ, boolean[][][] stoneV, RockState stoneC) {
+
         ArrayDeque<Pos> personQ = new ArrayDeque<>();
-        boolean[][] personV = new boolean[size][size];
-        int personY = stoneC.y + dy[stoneC.dir];
-        int personX = stoneC.x + dx[stoneC.dir];
-        personV[personY][personX]=true;
+        boolean[][] personV = new boolean[_SIZE][_SIZE];
+        int personY = stoneC.y + _DY[stoneC.dir];
+        int personX = stoneC.x + _DX[stoneC.dir];
+
+        if (_MAP[personY][personX] == '#' || _MAP[personY][personX] == 'D') return;
         personQ.add(new Pos(personY, personX));
-        while (!personQ.isEmpty()){
+        personV[personY][personX] = true;
+
+
+        int findCnt = 0;
+        if (_MAP[stoneC.y][stoneC.x + 1] == '#' || _MAP[stoneC.y][stoneC.x - 1] == '#') findCnt += 2;
+        if (_MAP[stoneC.y + 1][stoneC.x] == '#' || _MAP[stoneC.y - 1][stoneC.x] == '#') findCnt += 2;
+        if (findCnt == 4) return;
+        while (!personQ.isEmpty()) {
             Pos personC = personQ.poll();
-            for(int d=0; d<4; d++){
-                int ny = personC.y - dy[d];
-                int nx = personC.x - dx[d];
-                if(map[ny][nx] == '#') continue;
-                if(ny==stoneC.y && nx==stoneC.x){
-                    int nny = stoneC.y - dy[d];
-                    int nnx = stoneC.x - dx[d];
-                    if(map[nny][nnx]=='#') continue;
-                    if(stoneV[nny][nnx][d]) continue;
-                    stoneV[nny][nnx][d]=true;
+            for (int d = 0; d < 4; d++) {
+                int ny = personC.y - _DY[d];
+                int nx = personC.x - _DX[d];
+                if (ny < 1 || nx < 1 || ny > _SIZE - 1 || nx > _SIZE - 1) continue;
+
+                if (_MAP[ny][nx] == '#' || _MAP[ny][nx] == 'D') continue;
+                if (ny == stoneC.y && nx == stoneC.x) {
+                    int nny = stoneC.y - _DY[d];
+                    int nnx = stoneC.x - _DX[d];
+                    if (nny < 1 || nnx < 1 || nny > _SIZE - 1 || nnx > _SIZE - 1) continue;
+                    if (_MAP[nny][nnx] == '#') continue;
+                    if (stoneV[nny][nnx][d]) continue;
+                    stoneV[nny][nnx][d] = true;
                     stoneQ.add(new RockState(nny, nnx, d, stoneC.cnt + 1));
+
+                    findCnt++;
+                    if (findCnt == 4) continue;
+
                     continue;
                 }
-                if(personV[ny][nx]) continue;
-                personV[ny][nx]=true;
+                if (personV[ny][nx]) continue;
+                personV[ny][nx] = true;
                 personQ.add(new Pos(ny, nx));
 
             }
         }
     }
-    public static boolean personMove(int mPersonR, int mPersonC, int maroR, int maroC) {
-        boolean flag = false;
-        if(mPersonR == maroR && mPersonC == maroC) return true;
-        ArrayDeque<Pos> pmQ = new ArrayDeque<>();
-        boolean[][] pmV = new boolean[size][size];
-        pmV[mPersonR][mPersonC]=true;
-        pmQ.add(new Pos(mPersonR, mPersonC));
-        while (!pmQ.isEmpty()){
-            Pos personC = pmQ.poll();
-            for(int d=0; d<4; d++) {
-                int ny = personC.y - dy[d];
-                int nx = personC.x - dx[d];
-                if (map[ny][nx] == '#' || map[ny][nx] == 'R') continue;
-                if (pmV[ny][nx]) continue;
-                if (ny == maroR && nx == maroC) {
-                    flag=true;
-                }
-                pmV[ny][nx] = true;
-                pmQ.add(new Pos(ny, nx));
-            }
-        }
-        return flag;
-    }
 
-
-    static void print(int q, String cmd, int ans, int uans, Object... obj){
-        if(ans!=uans) System.out.println("====오답====");
-        System.out.println("["+q+"]" + cmd +"/"+ans+"="+uans+"/"+ Arrays.deepToString(obj));
-    }
-
-    private static int run(BufferedReader br) throws Exception {
-
-        int N;
-        boolean okay = false;
-        int p[] = new int[2]; //소년
-        int r[] = new int[2]; //바위
-        int d[] = new int[2]; //구멍
-
-        st = new StringTokenizer(br.readLine(), " ");
-        N = Integer.parseInt(st.nextToken());
-        char[][] region = new char[N][N];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            String rowStr = st.nextToken();
-            for (int j = 0; j < N; j++) {
-                region[i][j] = rowStr.charAt(j);
-                if(region[i][j] == 'D'){
-                    d = new int[]{i,j};
-                }else if(region[i][j] == 'R'){
-                    r = new int[]{i,j};
-                }else if(region[i][j] == 'P'){
-                    p = new int[]{i,j};
-                }
-
-            }
-        }
-        init(N, region);
-        int dir = 0;
-        boolean flag0 = personMove(p[0],p[1],r[0]-1,r[1]);
-        boolean flag2 = personMove(p[0],p[1],r[0]+1,r[1]);
-        boolean flag1 = personMove(p[0],p[1],r[0],r[1]+1);
-        boolean flag3 = personMove(p[0],p[1],r[0],r[1]-1);
-        if(flag2){
-            dir = 2;
-        }else if(flag1){
-            dir = 1;
-        }else if(flag3){
-            dir = 3;
-        }
-        return push(r[0], r[1], dir, d[0], d[1]);
-    }
 
     public static void main(String[] args) throws Exception {
-        System.setIn(new java.io.FileInputStream("input_b.txt"));
 
+        StringTokenizer st;
+        System.setIn(new java.io.FileInputStream("input_b.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stinit = new StringTokenizer(br.readLine(), " ");
 
         int TC = Integer.parseInt(stinit.nextToken());
-//        int MARK = Integer.parseInt(stinit.nextToken());
 
         for (int testcase = 1; testcase <= TC; ++testcase) {
-            int score = run(br);
-            System.out.println("#" + testcase + " " + score);
+
+            int rockX = 0; //바위 X 좌표
+            int rockY = 0; //바위 Y 좌표
+            int hollX = 0; //구멍
+            int hollY = 0; //구멍
+
+            st = new StringTokenizer(br.readLine(), " ");
+            _SIZE = Integer.parseInt(st.nextToken());
+            _MAP = new char[_SIZE][_SIZE];
+            for (int i = 0; i < _SIZE; i++) {
+                st = new StringTokenizer(br.readLine());
+                String rowStr = st.nextToken();
+                for (int j = 0; j < _SIZE; j++) {
+                    _MAP[i][j] = rowStr.charAt(j);
+                    if (_MAP[i][j] == 'D') {
+                        hollX = i;
+                        hollY = j;
+                    } else if (_MAP[i][j] == 'R') {
+                        rockX = i;
+                        rockY = j;
+                    }
+
+                }
+            }
+
+            // Rock BFS
+            int value = Integer.MAX_VALUE;
+            for (int i = 0; i < 4; i++) {
+                int move = push(rockX, rockY, i, hollX, hollY);
+                if (move != -1 && value > move) {
+                    value = move;
+                }
+            }
+            if(value == Integer.MAX_VALUE) value = -1;
+
+            System.out.println("#" + testcase + " " + value);
         }
         br.close();
     }
